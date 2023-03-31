c22da9d744127a787942082235c2529e79913aa0
Ujjal Kumar Roy <ujjalroy7862@gmail.comy>
2023-04-01 03:45:44 +0600

    make the components all pages and also store it in the index.js file for better codding

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CustomButton, Tab, ColorPicker, FilePicker, AIPicker } from '../components/index';
 
 const Customizer = () => {
     return <div>Customizer</div>;

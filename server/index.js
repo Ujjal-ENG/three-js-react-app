63e91304bab80f0a2ffbd0f273b301daeeef856d
Ujjal Kumar Roy <ujjalroy7862@gmail.comy>
2023-04-01 18:58:42 +0600

    after creating the server also install devependecy and make the first api

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import dallheRoutes from "./routes/dalle.route.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/dalle", dallheRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello From Dall" });
+});
+
+app.listen(8080, () =>
+  console.log(`Server is start at http://localhost:${8080}`)
+);

817dab6ccebc0f82d7052ab59e60e9909379f811
Ujjal Kumar Roy <ujjalroy7862@gmail.comy>
2023-04-01 13:53:31 +0600

    not done yet in the Tab

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useSnapshot } from 'valtio';
+import state from '../store';
 
-const Tab = () => {
-  return (
-    <div>Tab</div>
-  )
-}
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
+    const snap = useSnapshot(state);
 
-export default Tab
+    const activeStyles = isFilterTab && isActiveTab ? { backgroundColor: snap.color, opacity: 0.5 } : { backgroundColor: 'transparent', opacity: 1 };
+
+    return (
+        <div key={tab.name} className={`tab-btn ${isFilterTab ? 'rounded-full glassmorhism' : 'rounded-4'}`} onClick={handleClick} style={activeStyles}>
+            <img src={tab.icon} alt={tab.name} className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12'}`} />
+        </div>
+    );
+};
+
+export default Tab;
